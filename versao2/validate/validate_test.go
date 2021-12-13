99cd370b83ec6221f068be47deeae4dbf9e661e5
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2021-12-13 16:27:35 -0300

    added test in validate package

--- versao2/validate/validate_test.go
+++ versao2/validate/validate_test.go
@@ -4,22 +4,48 @@ import (
 	"testing"
 )
 
-var emails = []string{
+var emailsA = []string{
 	"teste1@teste.com",
 	"teste2@teste.com",
 	"teste3@teste.com",
 	"teste4@teste.com",
 }
 
+var emailsB = []string{
+	"teste1@teste.com",
+	"teste2@teste.com",
+	"teste3@teste.com",
+	"teste3@teste.com",
+}
+
+var emailsC = []string{
+	"teste1@teste.com",
+	"teste2@teste.com",
+	"",
+	"teste3@teste.com",
+}
+
 func TestValidateEmail(t *testing.T) {
 
-	t.Run("E-mail Ok", func(t *testing.T) {
-		got := ValidateEntries(emails)
-		want := false
-
+	validateEmails := func(t *testing.T, got, want bool) {
 		if got != want {
 			t.Errorf("got %v, want %v", got, want)
 		}
+	}
+
+	t.Run("E-mail Ok", func(t *testing.T) {
+		got := ValidateEntries(emailsA)
+		validateEmails(t, got, false)
+	})
+
+	t.Run("E-mail duplicado", func(t *testing.T) {
+		got := ValidateEntries(emailsB)
+		validateEmails(t, got, true)
+	})
+
+	t.Run("E-mail vazio", func(t *testing.T) {
+		got := ValidateEntries(emailsC)
+		validateEmails(t, got, true)
 	})
 
 }
