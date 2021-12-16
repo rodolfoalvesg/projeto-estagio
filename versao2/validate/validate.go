b5bd0285ed197b75d6bcda925c4e3a3d995da8fa
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2021-12-16 15:00:24 -0300

    update method validateEntries()

--- versao2/validate/validate.go
+++ versao2/validate/validate.go
@@ -11,18 +11,19 @@ type EmailsList struct {
 // validação de email repetido ou campo vazio
 func (e EmailsList) ValidateEntries() bool {
 
-	emailRepeated := map[string]string{}
+	emailRepeated := make(map[string]struct{}, len(e.Emails))
 
 	for _, entry := range e.Emails {
-		if _, ok := emailRepeated[entry]; !ok {
-			emailRepeated[entry] = ""
-		} else {
+		if _, ok := emailRepeated[entry]; ok {
 			return true
 		}
+
+		emailRepeated[entry] = struct{}{}
 
 		if entry == "" {
 			return true
 		}
 	}
+
 	return false
 }
